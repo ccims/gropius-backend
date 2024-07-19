4b49c9a5767b082d5a503f6edbf115c23d1809c6
nk-coding <niklask.coding@gmail.com>
2024-07-19 17:58:16 +0200

    move to public key jwt verification

--- api-public/src/main/kotlin/gropius/GropiusPublicApiConfigurationProperties.kt
+++ api-public/src/main/kotlin/gropius/GropiusPublicApiConfigurationProperties.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
  * Configuration properties for the public API
  *
  * @param debugNoAuthentication if `true`, all queries and most mutations don't require authentication to be present
- * @param jwtSecret secret used for JWT validation
+ * @param jwtPublicKey base64 encoded public key used for JWT validation
  */
 @ConfigurationProperties("gropius.api.public")
-data class GropiusPublicApiConfigurationProperties(val debugNoAuthentication: Boolean = false, val jwtSecret: String)
+data class GropiusPublicApiConfigurationProperties(val debugNoAuthentication: Boolean = false, val jwtPublicKey: String)
