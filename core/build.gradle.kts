71002d594049b27748dffff59601b5018019735b
nk-coding <niklask.coding@gmail.com>
2024-09-02 01:42:31 +0200

    add health endpoint

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,7 @@
 val graphglueVersion: String by project
 val graphqlJavaVersion: String by project
 val jsonSchemaValidatorVersion: String by project
+val springBootVersion: String by project
 
 plugins {
     kotlin("plugin.spring")
@@ -9,4 +10,5 @@ plugins {
 dependencies {
     api("io.github.graphglue", "graphglue-core", graphglueVersion)
     api("com.graphql-java","graphql-java-extended-scalars", graphqlJavaVersion)
+    implementation("org.springframework.boot", "spring-boot-starter-actuator", springBootVersion)
 }
